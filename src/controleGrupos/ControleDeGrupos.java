02b0b4c8d12f028bf9a0294431d992ddb83ac492
DanielleVieira <danielle.vieira@ccc.ufcg.edu.br>
2017-11-30 21:10:08 -0300

    Implementados testes da funcionalidade cadastrarGrupo em ControleDeGrupos

--- src/controleGrupos/ControleDeGrupos.java
+++ src/controleGrupos/ControleDeGrupos.java
@@ -1,5 +1,15 @@
 package controleGrupos;
 
+import java.util.Set;
+
 public class ControleDeGrupos {
 
+	
+	
+	private Set<Grupo> registroDeGrupos;
+
+	public boolean cadastrarGrupo(String nome) {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ testes/controleGrupos/ControleDeGruposTest.java
@@ -0,0 +1,48 @@
+
+package controleGrupos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import controleAlunos.ControleDeAlunos;
+
+
+public class ControleDeGruposTest {
+
+	
+	
+	private ControleDeGrupos cg1;
+
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		this.cg1 = new ControleDeGrupos();
+		this.cg1.cadastrarGrupo("Listas");
+	}
+
+	
+	
+	@Test
+	public void testCadastrarAluno() {
+		assertTrue(this.cg1.cadastrarGrupo("Cálculo"));
+		assertFalse(this.cg1.cadastrarGrupo("listas"));
+	}
+	
+	
+	
+	@Test(expected = NullPointerException.class)
+	public void testCadastrarNomeNull() {
+		this.cg1.cadastrarGrupo(null);
+	}
+	
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCadastrarNomeInvalido() {
+		this.cg1.cadastrarGrupo("");
+	}
+	
+}
